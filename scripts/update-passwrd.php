45f6093ae7d12e6b53769b74aed9991a8bfa5a9d
HamzaAmouch <9028844@student.zadkine.nl>
2026-05-13 13:59:15 +0200

    update
    update

--- scripts/update-passwrd.php
+++ scripts/update-passwrd.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <?php
-            $defaulPw = "w8woord";
+            $defaulPw = "XXLfrikandelbroodje";
             $hashedpw = password_hash($defaulPw, PASSWORD_DEFAULT);
 
             #1 verbinding met database
